eb16b1f31c592af1c6bfcf794c79c02b9e57449d
Guohui Xiao <xiao@inf.unibz.it>
2021-04-10 14:46:23 +0200

    get sorting back when generating log files

--- onprom-logextractor/src/main/java/it/unibz/inf/kaos/logextractor/SimpleEBDAReasoner.java
+++ onprom-logextractor/src/main/java/it/unibz/inf/kaos/logextractor/SimpleEBDAReasoner.java
@@ -255,10 +255,10 @@ class SimpleEBDAReasoner {
             logger.error(e.getMessage());
         }
 
-//        Collection<XTrace> traceList = traces.values();
-//        traceList.forEach(
-//                trace -> trace.sort(Comparator.comparing(e -> e.getAttributes().get("time:timestamp")))
-//        );
+        Collection<XTrace> traceList = traces.values();
+        traceList.forEach(
+                trace -> trace.sort(Comparator.comparing(e -> e.getAttributes().get("time:timestamp")))
+        );
 
         return traces.values();
     }
